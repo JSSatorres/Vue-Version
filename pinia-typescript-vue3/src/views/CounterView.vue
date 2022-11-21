ae8b408a66c8a22ea48a5522d3c2ae0f85ebf9a7
jssatorres <juansataz.dev@gmail.com>
2022-11-21 19:50:51 +0100

    feat Inital Component and view

---
+++ pinia-typescript-vue3/src/views/CounterView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Counter view</h1>
+  counter:{{ counterStore.count}}
+  {{" "}}
+  number of changes {{counterStore.numberOfChanges }} 
+  <button @click="counterStore.incrementBy(1)">+1</button>
+  <button @click="counterStore.incrementBy(2)">+2</button>
+  <button @click="counterStore.incrementBy(5)">+5</button>
+</template>
+
+<script setup lang="ts">
+import {useCounterStore} from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
---
+++ pinia-typescript-vue3/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar />
+  <h1>Home view: {{ counterStore.count}}</h1>
+</template>
+
+<script setup lang="ts">
+import Navbar from '../components/Navbar.vue'
+import {useCounterStore} from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+
+</script>
