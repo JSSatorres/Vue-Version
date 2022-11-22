6d6766691b30aceb3ebb80d3bf2fe1f134348fab
jssatorres <juansataz.dev@gmail.com>
2022-11-22 18:41:58 +0100

    Feat (Destiny) add new component

---
+++ pinia-typescript-vue3/src/components/Destinatiny.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h5> destination {{$route.params.id}}</h5>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  // computed:{
+  //   destanitaionId() {
+  //     return this.$route.params.id
+  //   }
+  
+
+
+</script>
+
+<style scoped>
+#nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin-top: 0;
+  padding: 30px;
+  background: #2c3e50;
+  color: white;
+  display: flex;
+  align-items: center;
+  border-bottom: 5px solid #4e6c71;
+  z-index: 2;
+}
+</style>
