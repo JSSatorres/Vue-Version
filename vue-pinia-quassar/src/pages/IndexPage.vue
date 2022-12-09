01c65f790f71e1a01e5db557f028d71f1d935fd0
jssatorres <juansataz.dev@gmail.com>
2022-12-09 18:50:34 +0100

    Feat (refreshToken) Wip

--- vue-pinia-quassar/src/pages/IndexPage.vue
+++ vue-pinia-quassar/src/pages/IndexPage.vue
@@ -1,22 +1,59 @@
 <template>
   <q-page padding>
     <q-btn @click="access"> Ingresar</q-btn>
+    <q-btn @click="createLink"> Create link</q-btn>
+    {{ token }}
   </q-page>
 </template>
 
 <script setup>
-import axios from "axios";
+import { api } from 'src/boot/axios'
+import {ref} from 'vue'
 
-const access = () => {
-  console.log("click");
-  axios
-    .post("http://localhost:5000/login", {
-      email: "algotragico@test.com",
-      password: "123456",
+const token = ref('')
+const expiresIn = ref('')
+
+const access = async () => {
+  try {
+    const res = await api.post("auth/login", {
+      email:"algotragico@test.com",
+      password: "123456"
     })
-    .then((res) => {
-      console.log(res.data);
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const refreshToken = async () =>{
+  try {
+    const res = await api.get("auth/refresh")
+    console.log(res.data);
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+  } catch (error) {
+    console.log(error);
+  }
+}
+const createLink = async () => {
+ try {
+  const res = await api({
+    method: 'POST',
+    url:'/link',
+    headers:{
+      'Authorization':'Bearer' + token.value 
+    },
+    data:{
+      longLink:"github.com/JSSatorres/Trello-App-Vue"
+    }
+    // 'links'
     })
-    .catch((error) => console.log(error));
-};
+
+    console.log(res);
+ } catch (error) {
+  console.log(error);
+ }
+}
+refreshToken()
 </script>
